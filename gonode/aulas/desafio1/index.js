44ea589b4e59acbd98c1c9a482c0aa223634e53a
PauloSpiguel <41386612+PauloSpiguel@users.noreply.github.com>
2019-04-24 07:07:04 -0300

    Aplicativo Desavio 01

--- gonode/aulas/desafio1/index.js
+++ gonode/aulas/desafio1/index.js
@@ -1,9 +1,53 @@
-const express = require('express');
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true
+})
+app.use(express.urlencoded({ express: false }))
+app.set('view engine', 'njk')
+
+const checkAgeQueryParam = (req, res, next) => {
+  const { age } = req.query
+
+  if (!age) {
+    return res.redirect('/')
+  }
+
+  return next()
+}
 
 app.get('/', (req, res) => {
-  return res.send('Desafio 01 - Modulo 01');
-});
+  return res.render('home')
+})
 
-app.listen(3000);
+app.get('/major', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+
+  return res.render('major', { age })
+})
+
+app.get('/minor', checkAgeQueryParam, (req, res) => {
+  const { age } = req.query
+
+  return res.render('minor', { age })
+})
+
+app.post('/check', (req, res) => {
+  const { age } = req.body
+  const hoje = new Date().getTime()
+  const dtNiver = new Date(age).getTime()
+
+  console.log(hoje - dtNiver)
+
+  if (age >= 18) {
+    return res.redirect(`/major?age=${age}`)
+  } else {
+    return res.redirect(`/minor?age=${age}`)
+  }
+})
+
+app.listen(3000)
