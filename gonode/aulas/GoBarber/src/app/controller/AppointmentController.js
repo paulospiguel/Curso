bd423b920f8896068aa79d2d022f116f9d1b261b
PauloSpiguel <41386612+PauloSpiguel@users.noreply.github.com>
2019-05-10 07:14:16 -0300

    Finalizando Agendamento

--- gonode/aulas/GoBarber/src/app/controller/AppointmentController.js
+++ gonode/aulas/GoBarber/src/app/controller/AppointmentController.js
@@ -1,11 +1,25 @@
 /* eslint-disable space-before-function-paren */
-const { User } = require('../models')
+const { User, Appointment } = require('../models')
 
 class AppointmentController {
   async create(req, res) {
     const provider = await User.findByPk(req.params.provider)
 
     return res.render('appointments/create', { provider })
+  }
+
+  async store(req, res) {
+    const { id } = req.session.user // Usuário que solicitou
+    const { provider } = req.params // O id do usuário (pela requisicão post)
+    const { date } = req.body // Valor do radio dentro da view available(index)
+
+    await Appointment.create({
+      user_id: id,
+      provider_id: provider,
+      date
+    })
+
+    return res.redirect('/app/dashboard')
   }
 }
 
