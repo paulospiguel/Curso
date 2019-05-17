32074cd5a1ea5cd777112cdfb69a16579f73cc84
PauloSpiguel <41386612+PauloSpiguel@users.noreply.github.com>
2019-05-17 19:12:00 -0300

    Criado Middleware de autenticação

--- gonode/aulas/Marketplace/src/routes.js
+++ gonode/aulas/Marketplace/src/routes.js
@@ -2,6 +2,9 @@ const express = require('express')
 
 const routes = express.Router()
 
+// ######### IMPORTS MIDDLEWARES ################
+const authMiddleware = require('./app/middlewares/auth')
+
 // ######### IMPORTS CONTROLLERS ################
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
@@ -9,5 +12,7 @@ const SessionController = require('./app/controllers/SessionController')
 // ######### ROUTES ################
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
 
 module.exports = routes
