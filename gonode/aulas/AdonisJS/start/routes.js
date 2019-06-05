5e1713b9c5db170935c4c70dfe16863310138d1b
PauloSpiguel <41386612+PauloSpiguel@users.noreply.github.com>
2019-06-04 21:10:37 -0300

    Implementação do Upload de arquivos

--- gonode/aulas/AdonisJS/start/routes.js
+++ gonode/aulas/AdonisJS/start/routes.js
@@ -6,3 +6,5 @@ Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 Route.post('forgot', 'ForgotPasswordController.store')
 Route.put('forgot', 'ForgotPasswordController.update')
+
+Route.post('/files', 'FileController.store')
