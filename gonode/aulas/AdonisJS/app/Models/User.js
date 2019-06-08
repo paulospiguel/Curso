d9e353feacbeaee5ee24d98aa0e3beb35b657161
PauloSpiguel <paulospil@hotmail.com>
2019-06-08 18:32:55 -0300

    CRUD - Project/Tasks

--- gonode/aulas/AdonisJS/app/Models/User.js
+++ gonode/aulas/AdonisJS/app/Models/User.js
@@ -9,7 +9,7 @@ const Model = use('Model')
 const Hash = use('Hash')
 
 class User extends Model {
-  static boot () {
+  static boot() {
     super.boot()
 
     this.addHook('beforeSave', async userInstance => {
@@ -19,8 +19,16 @@ class User extends Model {
     })
   }
 
-  tokens () {
+  tokens() {
     return this.hasMany('App/Models/Token')
+  }
+
+  projects() {
+    return this.hasMany('App/Models/Project')
+  }
+
+  tasks() {
+    return this.hasMany('App/Models/Task')
   }
 }
 
--- gonode/aulas/AdonisJS/start/routes.js
+++ gonode/aulas/AdonisJS/start/routes.js
@@ -8,4 +8,9 @@ Route.post('forgot', 'ForgotPasswordController.store')
 Route.put('forgot', 'ForgotPasswordController.update')
 
 Route.get('/files/:id', 'FileController.show')
-Route.post('/files', 'FileController.store')
+
+// Rotas com autenticação
+Route.group(() => {
+  Route.post('/files', 'FileController.store')
+  Route.resource('projects', 'ProjectController').apiOnly()
+}).middleware(['auth'])
