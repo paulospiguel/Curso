767c34aba1e129e34403de6c7b12ce6304d892b3
Paulo Spiguel <paulospil@hotmail.com>
2019-06-15 01:26:19 -0300

    Semana OmnineStack 7 - Desenvolvendo frontend - react

--- www/semana-omnistack-7/aulas/frontend/src/components/Header.js
+++ www/semana-omnistack-7/aulas/frontend/src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 import logo from "../assets/logo.svg";
-import camera from "../assets/logo.svg";
+import camera from "../assets/camera.svg";
 
 export default function Header() {
   return (
     <header id="main-header">
       <div className="header-content">
-        <img src={logo} alt="Rocketgram" />
-        <img src={camera} alt="Enviar publicação" />
+        <Link to="/">
+          <img src={logo} alt="Rocketgram" />
+        </Link>
+        <Link to="/new">
+          <img src={camera} alt="Enviar publicação" />
+        </Link>
       </div>
     </header>
   );
