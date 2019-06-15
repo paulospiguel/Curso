b0127a77ccc023e2367df54a4b88ef06eea883dc
Paulo Spiguel <paulospil@hotmail.com>
2019-06-15 17:36:42 -0300

    Omnistack 7 - Desenvolvimento React-Interface WEB

--- www/semana-omnistack-7/aulas/frontend/src/pages/Feed.js
+++ www/semana-omnistack-7/aulas/frontend/src/pages/Feed.js
@@ -1,8 +1,72 @@
 import React, { Component } from "react";
 
+import "./Feed.css";
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
+
 class Feed extends Component {
   render() {
-    return <h1>Feed</h1>;
+    return (
+      <section id="post-list">
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Paulo Spiguel</span>
+              <span className="place">Centenário do Sul</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+          <img
+            src="https://solabg.newtechtecnologia.com/wp-content/uploads/2019/04/economia.png"
+            alt="Mais"
+          />
+          <footer>
+            <div className="actions">
+              <img src={like} alt="Like" />
+              <img src={comment} alt="comment" />
+              <img src={send} alt="send" />
+            </div>
+
+            <strong> 900 curtidas</strong>
+            <p>
+              Um post muito legal da OmniStack
+              <span>#react #omnistack #top</span>
+            </p>
+          </footer>
+        </article>
+
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Paulo Spiguel</span>
+              <span className="place">Centenário do Sul</span>
+            </div>
+
+            <img src={more} alt="Mais" />
+          </header>
+          <img
+            src="https://solabg.newtechtecnologia.com/wp-content/uploads/2019/04/economia.png"
+            alt="Mais"
+          />
+          <footer>
+            <div className="actions">
+              <img src={like} alt="Like" />
+              <img src={comment} alt="comment" />
+              <img src={send} alt="send" />
+            </div>
+
+            <strong> 900 curtidas</strong>
+            <p>
+              Um post muito legal da OmniStack
+              <span>#react #omnistack #top</span>
+            </p>
+          </footer>
+        </article>
+      </section>
+    );
   }
 }
 
