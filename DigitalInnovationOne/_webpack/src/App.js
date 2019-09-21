c97cede3dcd54d78f034f55b099614fe55a913f1
Paulo Spiguel <paulospil@hotmail.com>
2019-09-21 12:12:25 +0100

    Criação Pasta Digital Innovation One

---
+++ DigitalInnovationOne/_webpack/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ color: "orange" }}>CURSO DIGITAL INNOVESION ONE</h1>
+      <h2>Criação de servidor com Webpack/Babel</h2>
+      <p>Paulo Spiguel</p>
+    </div>
+  );
+};
+
+export default App;
